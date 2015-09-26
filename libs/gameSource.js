785e6468872dd1b491e00fb114c28624a0e04c48
kamercer <kyle.mercer.km@gmail.com>
2015-09-26 02:47:00 -0400

    separate js file

--- libs/gameSource.js
+++ libs/gameSource.js
@@ -1,11 +1,13 @@
-var canvas = document.getElementById("gameCanvas");
-		var renderer = PIXI.autoDetectRenderer(1900, 900,{backgroundColor : 0x1099bb, view: canvas});
-		document.body.appendChild(renderer.view);
-		var stage = new PIXI.Container();
-		
-		animate();
-		function animate() 
-		{
-			requestAnimationFrame(animate);
-			renderer.render(stage);
-		}
+function init(){
+	var canvas = document.getElementById("gameCanvas");
+	var renderer = PIXI.autoDetectRenderer(1900, 900,{backgroundColor : 0x1099bb, view: canvas});
+	document.body.appendChild(renderer.view);
+	var stage = new PIXI.Container();
+	
+	animate();
+	function animate() 
+	{
+		requestAnimationFrame(animate);
+		renderer.render(stage);
+	}
+}
