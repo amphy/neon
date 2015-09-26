7469fa866852562a41f073d747eea627f744fb47
Lord and Master of All Things Big and Small <root@turing.iamdevnull.info>
2015-09-26 03:24:01 -0400

    added rectangle

--- js/gameSource.js
+++ js/gameSource.js
@@ -1,6 +1,6 @@
 function init(){
 	var canvas = document.getElementById("gameCanvas");
-	var renderer = PIXI.autoDetectRenderer(1900, 900,{backgroundColor : 0x1099bb, view: canvas});
+	var renderer = PIXI.autoDetectRenderer(1900, 900,{transparent: true, view: canvas});
 	document.body.appendChild(renderer.view);
 	var stage = new PIXI.Container();
 	
@@ -12,13 +12,16 @@ function init(){
 	}
 	
 	setUpStartScreen(stage);
+
 }
 
-function setUpStartScreen(){
+function setUpStartScreen(stage){
+  var graphics = new PIXI.Graphics();
   graphics.lineStyle(2, 0xFF00FF, 1);
   graphics.beginFill(0xFF00BB, 0.25);
   graphics.drawRoundedRect(150, 450, 300, 100, 15);
   graphics.endFill();	
+  stage.addChild(graphics);
 }
 
 function setUpCharacterScreen(stage){
